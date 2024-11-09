e5d6b6eb965c2ceacf74b97e5034f26de1b469d8
Roman Polek <polekroman@gmail.com>
2024-11-09 12:41:10 +0000

    Fixed parts of SingleApplication to pass the tests

--- src/SingleApplication.tsx
+++ src/SingleApplication.tsx
@@ -5,10 +5,11 @@ const SingleApplication = ({ application }) => {
 
   /**
    * Returns a string with the number formatted as currency in pounds and commas added
+   * Also rounds to 2 decimal places
    * @param number 
    */
   const formatNumber = (number: number): string => {
-    return `£${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`; 
+    return `£${(Math.round(number * 100) / 100).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`; 
   }
 
 
@@ -19,36 +20,49 @@ const SingleApplication = ({ application }) => {
   const formatDate = (date: Date): string => {
     //e.g. 10-12-2021
     //for some reason en-GB uses / as the separator but we want -
-    return date.toLocaleDateString('en-GB').replaceAll('/', '-');
+    const dateString = date.toLocaleDateString('en-GB').replaceAll('/', '-');
+    //if the date is invalid, return the string Invalid date value
+    if (dateString === 'Invalid Date') {
+      return 'Invalid date value';
+    }
+
+    return dateString;
+  }
+
+  let fullName = `${application.first_name ?? ''} ${application.last_name ?? ''}`.trim();
+  if (fullName === '') {
+    fullName = '-';
   }
 
   return (
     <div className={styles.SingleApplication}>
       <div className={styles.cell}>
         <sub>Company</sub>
-        <div className={styles.value}>{application.company}</div>
+        <div className={styles.value} data-testid="company">{application.company ?? '-'}</div>
       </div>
       <div className={styles.cell}>
         <sub>Name</sub>
-        <div className={styles.value}>{application.first_name} {application.last_name}</div>
+        <div className={styles.value} data-testid="name">{fullName}</div>
       </div>
       <div className={styles.cell}>
         <sub>Email</sub>
-        <a className={styles.value}
-          href={`mailto:${application.email}`}
-        >{application.email}</a>
+        <span data-testid="email">
+          {application.email? <a className={styles.value}
+            href={`mailto:${application.email}`}
+          >{application.email}</a> : '-'}
+        </span>
       </div>
       <div className={styles.cell}>
         <sub>Loan Amount</sub>
-        <div className={styles.value}>{formatNumber(application.loan_amount)}</div>
+        <div className={styles.value} data-testid="loan-amount">{application.loan_amount !== undefined ? formatNumber(application.loan_amount) : '-'}</div>
       </div>
       <div className={styles.cell}>
         <sub>Application Date</sub>
-        <div className={styles.value}>{formatDate(new Date(application.date_created))}</div>
+        <div className={styles.value} data-testid="date-created">{formatDate(new Date(application.date_created))}</div>
       </div>
       <div className={styles.cell}>
         <sub>Expiry date</sub>
-        <div className={styles.value}>{formatDate(new Date(application.expiry_date))}</div>
+        <div className={styles.value} data-testid="expiry-date">{formatDate(new Date(application.expiry_date))}</div>
       </div>
     </div>
   );
